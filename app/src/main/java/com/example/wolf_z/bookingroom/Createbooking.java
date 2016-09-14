b5e2349dc6a3c1fc02c133d0fdc6977ea64eb9fb
Saksukrit <kritzastep@gmail.com>
2016-09-14 16:12:04 +0700

    UI perform

--- app/src/main/java/com/example/wolf_z/bookingroom/Createbooking.java
+++ app/src/main/java/com/example/wolf_z/bookingroom/Createbooking.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,13 +30,21 @@ public class Createbooking extends Activity implements TimePickerDialog.OnTimeSe
     private Calendar calendar;
     private DateFormat dateFormat;
     private SimpleDateFormat timeFormat;
-    private Button setDate;
-    private Button setTime;
+    private Button btnsetDate;
+    private Button btnsetTime;
+    private Button btnsubmit;
+    private Animation anim;
+    private View view_subject;
+    private View view_detail;
+    private View view_search;
+    private EditText ETsubject;
+    private EditText ETdetail;
+    private EditText ETsearch;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.createbooking1);
+        setContentView(R.layout.createbooking);
 
         calendar = Calendar.getInstance();
         dateFormat = DateFormat.getDateInstance(DateFormat.LONG, Locale.getDefault());
@@ -42,19 +53,82 @@ public class Createbooking extends Activity implements TimePickerDialog.OnTimeSe
         txtdate = (TextView) findViewById(R.id.txtdate);
         txttime = (TextView) findViewById(R.id.txttime);
 
-        setDate = (Button) findViewById(R.id.setDate);
-        setTime = (Button) findViewById(R.id.setTime);
+        anim = AnimationUtils.loadAnimation(Createbooking.this, R.anim.scale);
+
+        view_subject = findViewById(R.id.view_subject);
+        view_detail = findViewById(R.id.view_detail);
+        view_search = findViewById(R.id.view_search);
+
+        ETsubject = (EditText) findViewById(R.id.ETsubject);
+        ETsubject.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    view_subject.setVisibility(View.VISIBLE);
+                    view_subject.startAnimation(anim);
+                } else {
+                    view_subject.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        ETdetail = (EditText) findViewById(R.id.ETdetail);
+        ETdetail.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    view_detail.setVisibility(View.VISIBLE);
+                    view_detail.startAnimation(anim);
+                } else {
+                    view_detail.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        ETsearch = (EditText) findViewById(R.id.ETsearch);
+        ETsearch.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (hasFocus) {
+                    view_search.setVisibility(View.VISIBLE);
+                    view_search.startAnimation(anim);
+                } else {
+                    view_search.setVisibility(View.GONE);
+                }
+            }
+        });
+
+        btnsetDate = (Button) findViewById(R.id.btnsetDate);
+        btnsetDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DatePickerDialog.newInstance(Createbooking.this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show(getFragmentManager(), "datePicker");
+            }
+        });
+
+        btnsetTime = (Button) findViewById(R.id.btnsetTime);
+        btnsetTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TimePickerDialog.newInstance(Createbooking.this, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show(getFragmentManager(), "timePicker");
+
+            }
+        });
+
+        btnsubmit = (Button) findViewById(R.id.btnsubmit);
+        btnsubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainBookingActivity.class);
+                startActivity(intent);
+                Toast.makeText(getApplicationContext(), "Create booking complete", Toast.LENGTH_LONG).show();
+                finish();
+            }
+        });
+
 
         update();
 
-    }
-
-    public void clicktime(View view) {
-        TimePickerDialog.newInstance(this, calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), true).show(getFragmentManager(), "timePicker");
-    }
-
-    public void clickdate(View view) {
-        DatePickerDialog.newInstance(this, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show(getFragmentManager(), "datePicker");
     }
 
 
@@ -81,10 +155,4 @@ public class Createbooking extends Activity implements TimePickerDialog.OnTimeSe
         update();
     }
 
-    public void submit(View view) {
-        Intent intent = new Intent(getApplicationContext(), MainBookingActivity.class);
-        startActivity(intent);
-        Toast.makeText(getApplicationContext(), "Create booking complete", Toast.LENGTH_LONG).show();
-        finish();
-    }
 }
